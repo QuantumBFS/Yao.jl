a576278031ab58a1b2a5305b995d3d1a56e573d0
autodocs <autodocs>
2018-04-17 10:25:45 +0000

    build based on 58095b1

---
+++ latest/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Quantum-Circuit-Simulation-for-Julia-1",
+    "page": "Home",
+    "title": "Quantum Circuit Simulation for Julia",
+    "category": "section",
+    "text": "Welcome to QuCircuit"
+},
+
+{
+    "location": "#Introduction-1",
+    "page": "Home",
+    "title": "Introduction",
+    "category": "section",
+    "text": "QuCircuit.jl is a quantum circuit simulator written in Julia."
+},
+
+]}
