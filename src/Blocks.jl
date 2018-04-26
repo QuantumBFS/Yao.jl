7358bc9b18f53d67bd2dc50fd6a80f99490e3d8d
Roger <hiroger@qq.com>
2018-04-26 19:26:57 +0800

    Register: remove unimplemented files

--- src/Blocks.jl
+++
@@ -1,9 +0,0 @@
-abstract type AbstractBlock{N} <: AbstractGate{N} end
-
-struct Block{N} <: AbstractBlock{N}
-    index::Vector
-    gates::Vector
-end
-
-apply!(block::AbstractBlock, pos::NTuple{N, Int}, gate) where N = 0
-apply!(block::AbstractBlock, pos::Tuple{}, gate) = 0
