2adee76fc400cf27bda529416d70976068c97db4
Roger <hiroger@qq.com>
2018-05-05 21:55:16 +0800

    improve tests

--- test/Blocks/Core.jl
+++ test/Blocks/Core.jl
@@ -1,4 +1,14 @@
 using Compat.Test
 
+import QuCircuit: is_size_match, AnySize
+
+@testset "test utils" begin
+
+    @test is_size_match(1, 1) == true
+    @test is_size_match(1, 2) == false
+    @test is_size_match(AnySize, 2) == true
+
+end
+
 include("Concentrator.jl")
 include("Sequence.jl")
