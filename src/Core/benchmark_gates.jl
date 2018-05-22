d4d242ad95bd20716b98e9780cdbc4898516fddf
Leo <cacate0129@gmail.com>
2018-05-23 04:40:27 +0800

    gate tests

--- src/Core/benchmark_gates.jl
+++ src/Core/benchmark_gates.jl
@@ -4,7 +4,7 @@ include("gates.jl")
 
 bss = basis(16)
 bg = BenchmarkGroup()
-bg["indices_with"] = @benchmarkable indices_with(2, bss)
+#bg["indices_with"] = @benchmarkable indices_with(2, bss)
 bg["X"] = @benchmarkable xgate(2, bss)
 bg["Y"] = @benchmarkable ygate(2, bss)
 bg["Z"] = @benchmarkable zgate(2, bss)
--- src/Core/test_gates.jl
+++ src/Core/test_gates.jl
@@ -14,4 +14,25 @@ include("gates.jl")
     end
 end
 
-# comparative test with respect to known kron results for n>1.
+# psi0
+# ['sx', 'sy', 'sz', 'rx(np.pi/6)', 'ry(np.pi/6)', 'rz(np.pi/6)', 'rot(np.pi/6, np.pi/3, np.pi/6)'] apply on 6
+# above gates controled by c(4)
+# above gates controled by c(2)nc(4)c(5)
+psi = loadldm("psi-test.jl")
+psi = psi[:, 1:2:end] + im * psi[:, 2:2:end]
+psi0 = psi[1,:]
+basis = collect(1:1<<num_bit)
+num_bit = 8
+# make following test pass
+gates = [xgate, ygate, zgate, rxgate(pi/6), rygate(pi/6), rzgate(pi/6), zxzrot(pi/6, pi/3, pi/6)]
+for gate, psii in zip(gates, psi[2:8])
+    @test $gate(6, basis)*psi0 == psii
+end
+cgates = [g|>c for g in gates]
+for gate, psii in zip(cgates, psi[9:15])
+    @test $gate(6, 4, basis)*psi0 == psii
+end
+cnccgates = [g|>c|>nc|>c for g in gates]
+for gate, psii in zip(cnccgates, psi[16:22])
+    @test $gate(6, 2,4,5, basis)*psi0 == psii
+end
