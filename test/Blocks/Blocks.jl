e3912025a33c88f2d8f8152d678e3673ce07594b
Roger-luo <hiroger@qq.com>
2018-05-25 20:59:42 +0800

    add test for concentrator & seq

--- test/Blocks/Blocks.jl
+++ test/Blocks/Blocks.jl
@@ -8,14 +8,14 @@ end
 include("MatrixBlock.jl")
 end
 
-# @testset "concentrator" begin
-# include("Concentrator.jl")
-# end
+@testset "concentrator" begin
+include("Concentrator.jl")
+end
 
-# @testset "sequence" begin
-# include("Sequence.jl")
-# end
+@testset "sequence" begin
+include("Sequence.jl")
+end
 
-# @testset "measure" begin
-# include("Measure.jl")
-# end
+@testset "measure" begin
+include("Measure.jl")
+end
