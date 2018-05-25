d33c47b1c5dab43206bfb259f4f26bdc1cecc81c
Roger-luo <hiroger@qq.com>
2018-05-26 00:38:24 +0800

    add new gate interface

--- src/PrettyInterface.jl
+++ src/PrettyInterface.jl
@@ -15,14 +15,20 @@ Create an instance of `gate_type`.
 create a Pauli X gate: `gate(X)`
 """
 gate(::Type{T}, ::Type{GT}) where {T, GT <: GateType} = Gate(T, GT)
-gate(::Type{T}, s::Symbol, params...) where T = gate(T, GateType{s}, params...)
+gate(::Type{T}, s::Symbol, params...) where T = gate(T, GateType{s}, promote(params...)...)
 
 # config default type
 gate(s::Symbol, params...) = gate(ComplexF64, s, params...)
 gate(::Type{GT}, params...) where {GT <: GateType} = gate(ComplexF64, GT, params...)
 
-gate(::Type{T}, ::Type{Val{:Ra}}, α::Complex{T}, β::Complex{T}, γ::Complex{T}) where T = chain(rot(:X, α), rot(:Z, β), rot(:X, γ))
+# define default promotion rule
+gate(::Type{Complex{T}}, ::Type{GT}, params...) where {T, GT} = gate(Complex{T}, GT, promote(T(first(params)), Base.tail(params)...)...)
 
+# define dispatch rules
+gate(::Type{Complex{T}}, ::Type{GateType{:Ra}}, α::T, β::T, γ::T) where T = chain(rot(:X, α), rot(:Z, β), rot(:X, γ))
+gate(::Type{Complex{T}}, ::Type{GateType{:Rx}}, theta::T) where T = rot(:X, theta)
+gate(::Type{Complex{T}}, ::Type{GateType{:Ry}}, theta::T) where T = rot(:Y, theta)
+gate(::Type{Complex{T}}, ::Type{GateType{:Rz}}, theta::T) where T = rot(:Z, theta)
 
 # 1.2 phase gate
 export phase
