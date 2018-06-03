0732fe549f1f4c7e60f0dab613fb94296d19cb8e
Leo <cacate0129@gmail.com>
2018-06-03 11:18:43 +0800

    new, projectq benchmark

--- benchmark/projectq/bench.py
+++ benchmark/projectq/bench.py
@@ -20,9 +20,20 @@ def bG(G):
     return lambda qureg: G|qureg[2]
 def bCG(G):
     return lambda qureg: ops.C(G)|(qureg[2], qureg[5])
+def bRot(G):
+    return lambda qureg: G(0.5)|qureg[2]
+def bCRot(G):
+    return lambda qureg: ops.C(G(0.5))|(qureg[2], qureg[5])
+def bToffoli():
+    return lambda qureg: ops.Toffoli |(qureg[2], qureg[3], qureg[5])
 def bRG(G):
     return lambda qureg: ops.Tensor(G)|qureg[2:8]
-    #return lambda qureg: G|qureg[2:8]
+def bGrover():
+    pass
+def bFFT():
+    pass
+def bTE():
+    pass
 
 NL = range(10, 28, 3)
 
@@ -43,6 +54,29 @@ class BenchMark():
                 tl.append(qbenchmark(func, nsite, num_bench)*1e6)
         np.savetxt('repeatxyz-report.dat', tl)
 
+    def hgate(self):
+        tl = []
+        for nsite, num_bench in zip(NL, [1000, 1000, 1000, 100, 10, 5]):
+            print('========== N: %d ============'%nsite)
+            for func in [bG(ops.H), bCG(ops.H), bRG(ops.H)]:
+                tl.append(qbenchmark(func, nsite, num_bench)*1e6)
+        np.savetxt('h-report.dat', tl)
+
+    def rot(self):
+        tl = []
+        for nsite, num_bench in zip(NL, [1000, 1000, 1000, 100, 10, 5]):
+            print('========== N: %d ============'%nsite)
+            for func in [bRot(ops.Rx), bRot(ops.Ry), bRot(ops.Rz), bCRot(ops.Rx), bCRot(ops.Ry), bCRot(ops.Rz)]:
+                tl.append(qbenchmark(func, nsite, num_bench)*1e6)
+        np.savetxt('rot-report.dat', tl)
+
+    def toffoli(self):
+        tl = []
+        for nsite, num_bench in zip(NL, [1000, 1000, 1000, 100, 10, 5]):
+            print('========== N: %d ============'%nsite)
+            for func in [bToffoli()]:
+                tl.append(qbenchmark(func, nsite, num_bench)*1e6)
+        np.savetxt('toffoli-report.dat', tl)
 
 if __name__ == '__main__':
     fire.Fire(BenchMark)
--- benchmark/projectq/plot.py
+++ benchmark/projectq/plot.py
@@ -2,37 +2,41 @@
 from plotlib import *
 import fire
 
+def _show_benchres(datafile, savefile, legends):
+    if isinstance(datafile, str):
+        data = np.loadtxt(datafile).reshape([6, len(legends)])
+    else:
+        data = datafile
+    with DataPlt(filename=savefile, figsize=(5,4)) as dp:
+        plt.plot(np.arange(10, 28, 3), data/1e3)
+        plt.legend(legends)
+        plt.yscale('log')
+        plt.ylabel(r'$t/ms$')
+        plt.xlabel(r'$N$')
+
 class PltBench():
     def xyz(self):
-        data = np.loadtxt('xyz-report.dat').reshape([6, 6])
-        with DataPlt(filename='projectq-xyz.png', figsize=(5,4)) as dp:
-            plt.plot(np.arange(10, 28, 3), data/1e3)
-            plt.legend(['X', 'Y', 'Z', 'CX', 'CY', 'CZ'])
-            plt.yscale('log')
-            plt.ylabel(r'$t/ms$')
-            plt.xlabel(r'$N$')
+        _show_benchres('xyz-report.dat', 'projectq-xyz.png', ['X', 'Y', 'Z', 'CX', 'CY', 'CZ'])
 
     def repeatxyz(self):
-        data = np.loadtxt('repeatxyz-report.dat').reshape([6, 3])
-        with DataPlt(filename='projectq-repeatxyz.png', figsize=(5,4)) as dp:
-            plt.plot(np.arange(10, 28, 3), data/1e3)
-            plt.legend(['X(2-7)', 'Y(2-7)', 'Z(2-7)'])
-            plt.yscale('log')
-            plt.ylabel(r'$t/ms$')
-            plt.xlabel(r'$N$')
-
+        _show_benchres('repeatxyz-report.dat', 'projectq-repeatxyz.png', ['X(2-7)', 'Y(2-7)', 'Z(2-7)'])
 
     def comparer(self):
         data = np.loadtxt('xyz-report.dat').reshape([6, 6])
         datar = np.loadtxt('repeatxyz-report.dat').reshape([6, 3])
-        with DataPlt(filename='projectq-comparerepeat.png', figsize=(5,4)) as dp:
-            plt.plot(np.arange(10, 28, 3), data[:,0]/1e3)
-            plt.plot(np.arange(10, 28, 3), datar[:,0]/1e3/6)
-            plt.legend(['X(2)', 'X(2-7) (time devided by 6)'])
-            plt.yscale('log')
-            plt.ylabel(r'$t/ms$')
-            plt.xlabel(r'$N$')
+        _show_benchres(np.concatenate([data[:,:1], datar[:,:1]/6], axis=1), 'projectq-comparerepeat.png', ['X(2)', 'X(2-7) (time devided by 6)'])
 
+    def hgate(self):
+        data = np.loadtxt('h-report.dat').reshape([6, 3])
+        data[:,2]/=6
+        print(data[:,2]/data[:,0])
+        _show_benchres(data, 'projectq-h.png', ['H', 'CH', 'H(2-7) (time / 6)'])
+
+    def toffoli(self):
+        _show_benchres('toffoli-report.dat', 'projectq-toffoli.png', ['toffoli'])
+
+    def rot(self):
+        _show_benchres('rot-report.dat', 'projectq-rot.png', ['Rx', 'Ry', 'Rz', 'C-Rx', 'C-Ry', 'C-Rz'])
 
 if __name__ == '__main__':
     fire.Fire(PltBench)
