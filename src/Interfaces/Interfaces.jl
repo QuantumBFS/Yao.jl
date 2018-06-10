05c969f01cfd035bfe716c9f6f5a57d973122c54
Roger-luo <hiroger@qq.com>
2018-06-10 09:31:28 +0800

    fix tests

--- src/Interfaces/Interfaces.jl
+++ src/Interfaces/Interfaces.jl
@@ -20,22 +20,34 @@ include("Composite.jl")
 include("Measure.jl")
 include("Cache.jl")
 
-export on, on!, with, with!
+export on, on!
 
-function with(f::Function, r::AbstractRegister)
-    f(copy(r))
+function apply_on!(r::AbstractRegister, block::AbstractBlock, params...; kwargs...)
+    apply!(r, block, params...; kwargs...)
 end
 
-function with!(f::Function, r::AbstractRegister)
-    f(r)
+function apply_on!(r::AbstractRegister, lf::Function, params...; kwargs...)
+    apply!(r, lf(nactive(r)), params...; kwargs...)
 end
 
-function on(r::AbstractRegister, params...)
-    block->apply!(copy(r), block, params...)
+"""
+    on(register, [params...]) -> f(block)
+
+Returns a lambda function that takes a block as its argument with
+configurations on a copy of this `register`.
+"""
+function on(r::AbstractRegister, params...; kwargs...)
+    x->apply_on!(copy(r), x, params...; kwargs...)
 end
 
-function on!(r::AbstractRegister, params...)
-    block->apply!(r, block, params...)
+"""
+    on!(register, [params...]) -> f(block)
+
+Returns a lambda function that takes a block as its argument with
+configurations on this `register` in place.
+"""
+function on!(r::AbstractRegister, params...; kwargs...)
+    x->apply_on!(r, x, params...; kwargs...)
 end
 
 export focus
--- test/Blocks/ChainBlock.jl
+++ test/Blocks/ChainBlock.jl
@@ -9,46 +9,36 @@ using Yao.Blocks
 @testset "constructor" begin
 
     g = ChainBlock(
-        kron(2, X(), Y()),
         kron(2, phase(0.1)),
-    )
-
-    @test g.blocks == [kron(2, X(), Y()), kron(2, phase(0.1))]
-end
-
-@testset "functionality" begin
-    g = ChainBlock(
         kron(2, X(), Y()),
-        kron(2, phase(0.1))
     )
 
-    m = mat(kron(2, phase(0.1))) * mat(kron(2, X, Y))
-    @test mat(g) ≈ m
-    @test mat(g) ≈ m
-
-    reg = rand_state(2)
-    @test m * state(reg) == state(apply!(reg, g))
-
+    @test g isa ChainBlock{2, ComplexF64} # default type
+    @test g.blocks == [kron(2, phase(0.1)), kron(2, X(), Y())]
 end
 
-@testset "allocation" begin
-    g = ChainBlock(X(), phase(0.1))
+@testset "matrix" begin
+g = ChainBlock(
+    kron(2, phase(0.1)),
+    kron(2, X, Y),
+)
 
-    cg = copy(g)
-    cg[2].theta = 0.2
-    @test g[2].theta == 0.1
+m = mat(kron(2, phase(0.1))) * mat(kron(2, X, Y))
+@test mat(g) ≈ m
+end
 
-    sg = similar(g)
-    @test_throws BoundsError sg[2]
+@testset "apply" begin
+g = ChainBlock(
+    kron(2, phase(0.1)),
+    kron(2, X, Y),
+)
 
-    sg[1] = X()
-    @test sg[1] == X()
-    g[1] = Y()
-    @test g[1] == Y()
+reg = rand_state(2)
+@test statevec(g |> on(reg)) ≈ mat(g) * reg
 end
 
 @testset "iteration" begin
-    test_list = [X(), Y(), phase(0.1), rot(X)]
+    test_list = [rot(X), phase(0.1), Y, X]
     g = ChainBlock(test_list)
 
     for (src, tg) in zip(g, test_list)
@@ -61,9 +51,9 @@ end
 end
 
 @testset "additional" begin
-    g = ChainBlock(X(), Y())
-    push!(g, Z())
-    @test g[3] == Z()
+    g = ChainBlock(Y, X)
+    push!(g, Z)
+    @test g[3] == Z
 
     append!(g, [rot(X), rot(Y)])
     @test g[4] == rot(X)
@@ -71,14 +61,14 @@ end
 
     prepend!(g, [phase(0.1)])
     @test g[1] == phase(0.1)
-    @test g[2] == X()
+    @test g[2] == Y
     @test g[end] == rot(Y)
 end
 
 @testset "traits" begin
     # TODO: check traits when primitive blocks' traits are all defined
 
-    g = ChainBlock(X(), Y())
+    g = ChainBlock(X, Y)
     @test isunitary(g) == true
     @test isreflexive(g) == false
     @test ishermitian(g) == false
