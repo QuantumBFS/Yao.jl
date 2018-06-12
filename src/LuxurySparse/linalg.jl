ecd74c2a7fa696d70aa2a335cd8ca0147bd1856e
Leo <cacate0129@gmail.com>
2018-06-13 03:48:22 +0800

    fix 0.7 test

--- src/LuxurySparse/linalg.jl
+++ src/LuxurySparse/linalg.jl
@@ -123,22 +123,30 @@ for MTYPE in [PermMatrix, IMatrix]
 end # ex
 =#
 
-*(x::Adjoint{<:Any,<:AbstractVector}, D::PermMatrix) = RowVector(conj(parent(x)))*D
-*(x::Transpose{<:Any,<:AbstractVector}, D::PermMatrix) = RowVector(parent(x))*D
+*(x::Adjoint{<:Any,<:AbstractVector}, D::PermMatrix) = Matrix(x)*D
+*(x::Transpose{<:Any,<:AbstractVector}, D::PermMatrix) = Matrix(x)*D
 *(A::Adjoint{<:Any,<:AbstractArray}, D::PermMatrix) = Adjoint(adjoint(D)*parent(A))
 *(A::Transpose{<:Any,<:AbstractArray}, D::PermMatrix) = Transpose(transpose(D)*parent(A))
 *(A::Adjoint{<:Any,<:PermMatrix}, D::PermMatrix) = adjoint(parent(A))*D
 *(A::Transpose{<:Any,<:PermMatrix}, D::PermMatrix) = transpose(parent(A))*D
 *(A::PermMatrix, D::Adjoint{<:Any,<:PermMatrix}) = A*adjoint(parent(D))
 *(A::PermMatrix, D::Transpose{<:Any,<:PermMatrix}) = A*transpose(parent(D))
+for MAT in [:AbstractArray, :Matrix, :SparseMatrixCSC, :PermMatrix]
+    @eval *(A::Adjoint{<:Any,<:$MAT}, D::PermMatrix) = copy(A)*D
+    @eval *(A::Transpose{<:Any,<:$MAT}, D::PermMatrix) = copy(A)*D
+    @eval *(A::PermMatrix, D::Adjoint{<:Any,<:$MAT}) = A*copy(D)
+    @eval *(A::PermMatrix, D::Transpose{<:Any,<:$MAT}) = A*copy(D)
+end
 
 ############### Transpose, Adjoint for IMatrix ###############
-for MAT in [:AbstractArray, :AbstractVector, :PermMatrix, :IMatrix]
-    @eval *(x::Adjoint{<:Any,<:$MAT}, D::IMatrix) = Adjoint(D*parent(x))
-    @eval *(x::Transpose{<:Any,<:$MAT}, D::IMatrix) = Transpose(D*parent(x))
+for MAT in [:AbstractArray, :AbstractVector, :Matrix, :SparseMatrixCSC, :PermMatrix, :IMatrix]
+    @eval *(A::Adjoint{<:Any,<:$MAT}, D::IMatrix) = Adjoint(D*parent(A))
+    @eval *(A::Transpose{<:Any,<:$MAT}, D::IMatrix) = Transpose(D*parent(A))
+    if MAT != :AbstactVector
+        @eval *(A::IMatrix, D::Transpose{<:Any,<:$MAT}) = Transpose(parent(D)*A)
+        @eval *(A::IMatrix, D::Adjoint{<:Any,<:$MAT}) = Adjoint(parent(D)*A)
+    end
 end
-*(A::IMatrix, D::Transpose{<:Any,<:IMatrix}) = A*D
-*(A::IMatrix, D::Adjoint{<:Any,<:IMatrix}) = A*D
 
 end
 
