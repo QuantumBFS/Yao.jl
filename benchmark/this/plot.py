fe3e77fb048d295b9f451ab7c3804790fd3fcb90
Leo <cacate0129@gmail.com>
2018-06-14 16:25:15 +0800

    new benchmarks

---
+++ benchmark/this/plot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from plotlib import *
+import fire
+
+def _show_benchres(datafile, savefile, legends):
+    if isinstance(datafile, str):
+        data = np.loadtxt(datafile).reshape([6, len(legends)])
+    else:
+        data = datafile
+    with DataPlt(filename=savefile, figsize=(5,4)) as dp:
+        plt.plot(np.arange(10, 28, 3), data/1e6)
+        plt.legend(legends)
+        plt.yscale('log')
+        plt.ylabel(r'$t/ms$')
+        plt.xlabel(r'$N$')
+        plt.ylim(1e-1, 1e2)
+
+class PltBench():
+    def xyz(self):
+        _show_benchres('xyzcxyz.dat', 'yao-xyz.png', ['X', 'CX', 'Y', 'CY', 'Z', 'CZ'])
+
+    def repeatxyz(self):
+        _show_benchres('repeatxyz-report.dat', 'projectq-repeatxyz.png', ['X(2-7)', 'Y(2-7)', 'Z(2-7)'])
+
+    def comparer(self):
+        data = np.loadtxt('xyz-report.dat').reshape([6, 6])
+        datar = np.loadtxt('repeatxyz-report.dat').reshape([6, 3])
+        _show_benchres(np.concatenate([data[:,:1], datar[:,:1]/6], axis=1), 'projectq-comparerepeat.png', ['X(2)', 'X(2-7) (time devided by 6)'])
+
+    def hgate(self):
+        data = np.loadtxt('h-report.dat').reshape([6, 3])
+        data[:,2]/=6
+        print(data[:,2]/data[:,0])
+        _show_benchres(data, 'projectq-h.png', ['H', 'CH', 'H(2-7) (time / 6)'])
+
+    def toffoli(self):
+        _show_benchres('toffoli-report.dat', 'projectq-toffoli.png', ['toffoli'])
+
+    def rot(self):
+        _show_benchres('rot-report.dat', 'projectq-rot.png', ['Rx', 'Ry', 'Rz', 'C-Rx', 'C-Ry', 'C-Rz'])
+
+if __name__ == '__main__':
+    fire.Fire(PltBench)
--- benchmark/this/xyz.jl
+++ benchmark/this/xyz.jl
@@ -8,16 +8,18 @@ with!(reg) = reg
 with(reg) = copy(reg)
 |>(reg::AbstractRegister, blk::AbstractBlock) = apply!(reg, blk)
 
-nbit = 16
-res_list = Float64[]
-reg = rand_state(16)
-for G in [X, Y, Z]
-    # single gate
-    g3 = repeat(nbit, G, [3])
-    res = @benchmark $reg |> $g3
-    push!(res_list, median(res).time)
-    c7g3 = control(nbit, (7,), 3=>G)
-    res = @benchmark $reg |> $c7g3
-    push!(res_list, median(res).time)
+res_list = zeros(6, 6)
+for (ib, nbit) in enumerate(10:3:25)
+    reg = rand_state(nbit)
+    for (ig, G) in enumerate([X, Y, Z])
+        # single gate
+        g3 = repeat(nbit, G, [3])
+        res = @benchmark $reg |> $g3
+        res_list[ib, 2*ig-1] = median(res).time
+        c7g3 = control(nbit, (7,), 3=>G)
+        res = @benchmark $reg |> $c7g3
+        res_list[ib, 2*ig] = median(res).time
+    end
 end
 println(res_list)
+writedlm("xyzcyxz.dat", res_list)
