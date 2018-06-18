7ddf3fd7ef88acd712c3ffabebe3f805d4ec6ce2
Leo <cacate0129@gmail.com>
2018-06-19 01:59:06 +0800

    redo repeat, turn addrs to tuples

--- src/Boost/Repeated.jl
+++ src/Boost/Repeated.jl
@@ -2,13 +2,12 @@ GATES = [:X, :Y, :Z]
 
 for (G, MATFUNC) in zip(GATES, [:xgate, :ygate, :zgate])
     GGate = Symbol(G, :Gate)
-    @eval function mat(rb::RepeatedBlock{N, MT, GT}) where {N, MT, GT <: $GGate}
-        $MATFUNC(MT, N, rb.addrs)
+    @eval function mat(rb::RepeatedBlock{N, C, GT, MT}) where {N, C, MT, GT <: $GGate}
+        $MATFUNC(MT, N, [rb.addrs...])
     end
 end
 
-apply!(r::AbstractRegister{B}, rb::RepeatedBlock{N, T, <:MatrixBlock{1}}) where {B, N, T} = (u1apply!(r.state |> matvec, mat(rb.block), rb.addrs[]); r)
-mat(rb::RepeatedBlock{N}) where N = hilbertkron(N, fill(mat(rb.block), length(rb.addrs)), rb.addrs)
+apply!(r::AbstractRegister{B}, rb::RepeatedBlock{N, 1, <:MatrixBlock{1}}) where {B, N} = (u1apply!(r.state |> matvec, mat(rb.block), rb.addrs...); r)
 for (GATE, METHOD) in zip([:XGate, :YGate, :ZGate], [:xapply!, :yapply!, :zapply!])
-    @eval apply!(r::AbstractRegister{B}, rb::RepeatedBlock{N, T, <:$GATE}) where {B, N, T} = ($METHOD(r.state |> matvec, rb.addrs); r)
+    @eval apply!(r::AbstractRegister{B}, rb::RepeatedBlock{N, C, <:$GATE}) where {B, N, C} = ($METHOD(r.state |> matvec, [rb.addrs...]); r)
 end
