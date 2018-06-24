ce19cf0f160d57fb6d2d147c2afcc88b52a81dc1
Leo <cacate0129@gmail.com>
2018-06-25 00:21:20 +0800

    new rotbasis

--- src/Zoo/Zoo.jl
+++ src/Zoo/Zoo.jl
@@ -3,6 +3,10 @@ module Zoo
 using ..Yao
 using ..Blocks
 using ..Registers
+using ..LuxurySparse
+import ..Blocks: mat, dispatch!, nparameters, parameters, cache_key, print_block, _make_rot_mat
+import Base: ==, copy, hash
+
 
 # Block APIs
 export QFT
@@ -10,5 +14,6 @@ export diff_circuit, num_gradient, rotter, cnot_entangler, opgrad, collect_rotbl
 
 include("QFT.jl")
 include("Differential.jl")
+include("RotBasis.jl")
 
 end
