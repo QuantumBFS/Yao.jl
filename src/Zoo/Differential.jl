04425d6a983caaa8b50d64e45f4dfb6a50203c85
Leo <cacate0129@gmail.com>
2018-06-28 17:30:35 +0800

    fix differential

--- src/Zoo/Differential.jl
+++ src/Zoo/Differential.jl
@@ -49,43 +49,49 @@ function collect_rotblocks(blk::AbstractBlock)
     rots
 end
 
-import Base: gradient
-function _wave_perturbation(circuit::Pair{<:AbstractRegister, <:AbstractBlock}, gate::RotationGate, diff::Real)
-    reg0, block = circuit
-    dispatch!(+, gate, diff)
-    psi_pos = copy(reg0) |> block
+"""
+    perturb(func, gates::Vector{<:RotationGate}, diff::Real) -> Matrix
+    
+perturb every rotation gates, and evaluate losses.
+The i-th element of first column of resulting Matrix corresponds to Gi(θ+δ), and the second corresponds to Gi(θ-δ).
+"""
+function perturb(func, gates::Vector{<:RotationGate}, diff::Real)
+    ng = length(gates)
+    res = Matrix{Float64}(ng, 2)
+    for i in 1:ng
+        gate = gates[i]
+        dispatch!(+, gate, diff)
+        res[i, 1] = func()
 
-    dispatch!(+, gate, -2*diff)
-    psi_neg = copy(reg0) |> block
+        dispatch!(+, gate, -2*diff)
+        res[i, 2] = func()
 
-    dispatch!(+, gate, diff) # set back
-    psi_pos, psi_neg
+        dispatch!(+, gate, diff) # set back
+    end
+    res
 end
 
 """
-    gradient(gradfunc, circuit::Pair{<:AbstractRegister, <:AbstractBlock}, gates::Vector{RotationGate}) -> Vector
-"""
-function gradient(gradfunc, circuit::Pair{<:AbstractRegister, <:AbstractBlock}, gates::Vector{RotationGate})
-    map(gate->gradfunc(_wave_perturbation(circuit, gate, π/2)...), gates)
-end
-
-"""
-    num_gradient(lossfunc, circuit::Pair{<:AbstractRegister, <:AbstractBlock}, gates::Vector{RotationGate}, δ::Float64=1e-2) -> Vector
+    num_gradient(lossfunc, rots::Vector{<:RotationGate}, δ::Float64=1e-2) -> Vector
 
 Compute gradient numerically.
 """
-function num_gradient(lossfunc, circuit::Pair{<:AbstractRegister, <:AbstractBlock}, gates::Vector{RotationGate}, δ::Float64=1e-2)
-    map(gates) do gate
-        rp, rn = _wave_perturbation(circuit, gate, δ)
-        (lossfunc(rp) - lossfunc(rn))/2/δ
-    end
+function num_gradient(lossfunc, rots::Vector{<:RotationGate}, δ::Float64=1e-2)
+    gperturb = perturb(lossfunc, rots, δ)
+    (gperturb[:,1] - gperturb[:,2])/(2δ)
 end
 
-opgradfunc(op) = (reg_pos, reg_neg) -> (expect(op, reg_pos)-expect(op, reg_neg))/2
-
 """
-    opgrad(op::AbstractBlock, circuit::Pair{<:AbstractRegister, <:AbstractBlock}, gates::Vector{RotationGate}) -> Vector
+    opgrad(op_expect, rots::Vector{<:RotationGate}) -> Vector
 
-get the gradient of an operator, which should be an observable.
+get the gradient of an operator expectation function.
+
+References:
+    Mitarai, K., Negoro, M., Kitagawa, M., & Fujii, K. (2018). Quantum Circuit Learning, 1–3. Retrieved from http://arxiv.org/abs/1803.00745
 """
-opgrad(op::AbstractBlock, circuit::Pair{<:AbstractRegister, <:AbstractBlock}, gates::Vector{RotationGate}) = gradient(opgradfunc(op), circuit, gates)
+function opgrad(op_expect, rots::Vector{<:RotationGate})
+    gperturb = perturb(op_expect, rots, π/2)
+    (gperturb[:,1] - gperturb[:,2])/2
+end
+
+
