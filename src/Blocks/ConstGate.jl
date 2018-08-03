5183bc87f0820ba742e0bc8c2b5772d6a3c3e66c
Roger-luo <hiroger@qq.com>
2018-08-03 23:28:18 +0800

    rm v0.6 const gate tools, this was once dangerous

--- src/Blocks/ConstGate.jl
+++ src/Blocks/ConstGate.jl
@@ -7,12 +7,7 @@ Abstract type for constant gates.
 """
 abstract type ConstantGate{N, T} <: PrimitiveBlock{N, T} end
 
-@static if VERSION < v"0.7-"
-    include("ConstGateTools.jl")
-else
-    include("ConstGateTools2.jl")
-end
-
+include("ConstGateTools.jl")
 cache_key(x::ConstantGate) = 0x1
 
 include("ConstGateGen.jl")
