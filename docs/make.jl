f5b1f2d9a9d13f764cd34f2ba19d4d45bcfe7e27
Roger-luo <hiroger@qq.com>
2018-08-19 01:50:21 +0800

    add docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,56 @@
 using Pkg
 
-pkg"""
-add Documenter Literate
-"""
+# pkg"""
+# add Documenter Literate
+# """
 
 using Documenter, Literate, QuAlgorithmZoo
 
-const TUTORIAL = joinpath(@__DIR__, "src/tutorial")
-const EXAMPLES = joinpath(@__DIR__, "..", "examples")
+const PATH = (
+    tutorial = joinpath(@__DIR__, "src/tutorial"),
+    examples = joinpath(@__DIR__, "..", "examples")
+)
 
-for (root, dirs, files) in walkdir(EXAMPLES)
-    for file in files
-        println(joinpath(root, file))
+function process_literate_scripts(;excludes=["make.jl", "README.md"])
+    TUTORIALS = []
+    for (root, dirs, files) in walkdir(PATH.examples)
+        for file in files
+            file in excludes && continue
+            filepath = joinpath(root, file)
+            Literate.markdown(filepath, PATH.tutorial)
+
+            filename, _ = splitext(file)
+            mdfile = join([filename, ".md"])
+            # TODO: use PATH.tutorial rather then manual path
+            push!(TUTORIALS, relpath(joinpath("tutorial", mdfile)))
+        end
     end
+    TUTORIALS
 end
 
-# Literate.markdown(EXAMPLES, TUTORIAL)
+const TUTORIALS = process_literate_scripts()
+
+#-----------------------------------------------
+
+makedocs(
+    modules = [QuAlgorithmZoo],
+    clean = false,
+    format = :html,
+    sitename = "Quantum Algorithm Zoo",
+    linkcheck = !("skiplinks" in ARGS),
+    analytics = "UA-89508993-1",
+    pages = [
+        "Home" => "index.md",
+        "Tutorial" => TUTORIALS,
+    ],
+    html_prettyurls = !("local" in ARGS),
+    html_canonical = "https://quantumbfs.github.io/QuAlgorithmZoo.jl/latest/",
+)
+
+deploydocs(
+    repo = "github.com/QuantumBFS/QuAlgorithmZoo.jl.git",
+    target = "build",
+    julia = "1.0",
+    deps = nothing,
+    make = nothing,
+)
---
+++ docs/src/index.md
@@ -0,0 +1,7 @@
+```@meta
+CurrentModule = QuAlgorithmZoo
+```
+
+# Quantum Algorithm Zoo
+
+A curated implementation of quantum algorithms with [Yao.jl](https://github.com/QuantumBFS/Yao.jl)
