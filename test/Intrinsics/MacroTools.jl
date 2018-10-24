3b36f21a468eef06fe351a9d6b31198dff7251d7
Roger-luo <hiroger@qq.com>
2018-10-24 13:23:55 -0400

    fix tests

--- test/Intrinsics/MacroTools.jl
+++ test/Intrinsics/MacroTools.jl
@@ -9,14 +9,15 @@ using Yao.Intrinsics
     @test_throws AddressConflictError assert_addr_safe(N, [2:5, 1:3])
     @test_throws AddressConflictError assert_addr_safe(N, [1:3, 2:9])
 end
+
 @testset "assert addr fit" begin
-    @test @assert_addr_fit 8 [1:3, 7:8, 4:5, 6:6]
-    @test_throws AddressConflictError @assert_addr_fit 8 [1:3, 7:7, 4:5]
-    @test_throws AddressConflictError @assert_addr_fit 8 [1:3, 2:9]
+    @test assert_addr_fit 8 [1:3, 7:8, 4:5, 6:6]
+    @test_throws AddressConflictError assert_addr_fit 8 [1:3, 7:7, 4:5]
+    @test_throws AddressConflictError assert_addr_fit 8 [1:3, 2:9]
 end
 
 @testset "assert addr inbounds" begin
-    @test @assert_addr_inbounds 8 [1:3, 7:7, 4:5]
-    @test @assert_addr_inbounds 8 [2:5, 1:3]
-    @test_throws AddressConflictError @assert_addr_inbounds 8 [2:9, 1:3]
+    @test assert_addr_inbounds 8 [1:3, 7:7, 4:5]
+    @test assert_addr_inbounds 8 [2:5, 1:3]
+    @test_throws AddressConflictError assert_addr_inbounds 8 [2:9, 1:3]
 end
