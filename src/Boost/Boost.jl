3b0f644ce37d310ce9fb6e7437e44d5779bad90a
Leo <cacate0129@gmail.com>
2018-10-28 02:39:11 +0800

    boost T gates and S gates

--- src/Boost/Boost.jl
+++ src/Boost/Boost.jl
@@ -9,12 +9,12 @@ using LinearAlgebra
 import ..Blocks: mat, apply!
 import ..Intrinsics: u1apply!, unapply!, general_controlled_gates, general_c1_gates, unrows!
 
-export xgate, ygate, zgate
-export cxgate, cygate, czgate
+export xgate, ygate, zgate, tgate, tdaggate, sgate, sdaggate
+export cxgate, cygate, czgate, ctgate, ctdaggate, csgate, csdaggate
 export controlled_U1, controller
 
-export xapply!, yapply!, zapply!, u1apply!
-export cxapply!, cyapply!, czapply!
+export xapply!, yapply!, zapply!, tapply!, tdagapply!, sapply!, sdagapply!, u1apply!
+export cxapply!, cyapply!, czapply!, ctapply!, ctdagapply!, csapply!, csdagapply!
 
 include("gates.jl")
 include("applys.jl")
