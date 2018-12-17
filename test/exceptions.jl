36b94ccbe67cc79af4cf7b1803f1a6effb46d0ce
Roger-luo <hiroger@qq.com>
2018-12-17 18:16:45 -0500

    fix tests

--- test/exceptions.jl
+++ test/exceptions.jl
@@ -2,6 +2,5 @@ using Test, YaoBase, YaoBase.TestUtils
 
 @testset "test exception msg" begin
     @test_io NotImplementedError(:nqubits) """
-    nqubits is not implemented.
-    """
+    nqubits is not implemented."""
 end
