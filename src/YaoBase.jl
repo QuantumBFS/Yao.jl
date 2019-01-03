d442f7e7a57242c674ed299d45df22ba5ad72732
Roger-luo <hiroger@qq.com>
2019-01-03 17:40:39 +0800

    add instruct

--- src/YaoBase.jl
+++ src/YaoBase.jl
@@ -7,11 +7,11 @@ using LinearAlgebra, LuxurySparse
 
 include("exceptions.jl")
 include("inspect.jl")
+include("instruct.jl")
 
 include("utils/interface.jl")
 include("abstract_register.jl")
 include("adjoint_register.jl")
-
 include("utils/test_utils.jl")
 
 end # module
