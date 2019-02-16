b1f227bc83002481ee6e3c55bdc402483256180a
Roger-luo <hiroger@qq.com>
2019-02-16 14:35:21 +0800

    add empty instruction

--- src/instruct.jl
+++ src/instruct.jl
@@ -9,3 +9,8 @@ This operator will be overloaded for different operator or state with
 different types.
 """
 function instruct! end
+
+# empty gates
+YaoBase.instruct!(state::AbstractVecOrMat, ::Any,
+    locs::Tuple{}, control_bits::NTuple{N1, Int}=(),
+    control_vals::NTuple{N2, Int}=()) where {N1, N2} = state
