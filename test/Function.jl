632a75f1eecc4dd94e64994fc266360ea9706473
Roger-luo <hiroger@qq.com>
2019-02-23 14:59:33 +0800

    rm old files

--- test/Function.jl
+++
@@ -1,9 +0,0 @@
-using Test, Random, LinearAlgebra, SparseArrays
-
-using YaoBase, YaoBlockTree
-
-@testset "functor" begin
-    Test_InvOrder = FunctionBlock(invorder!)
-    reg = rand_state(4)
-    @test copy(reg) |> invorder! == apply!(copy(reg), Test_InvOrder)
-end
