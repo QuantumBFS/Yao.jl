d9a46eb5efd8b73385c609a7fe4d384a53e8e9c3
Roger-luo <hiroger@qq.com>
2019-02-23 14:57:53 +0800

    clean up exports and imports

--- src/YaoBlockTree.jl
+++ src/YaoBlockTree.jl
@@ -1,48 +1,14 @@
 module YaoBlockTree
 
-using Random, LinearAlgebra, SparseArrays
-using MacroTools: @forward
-using LinearMaps, DataStructures, CacheServers, LuxurySparse
-using Expokit: expmv
-
-using YaoBase, YaoDenseRegister, YaoBase.Basis, YaoBase.Math
-using YaoDenseRegister: matvec
-using YaoBase.Math: linop2dense
-using TupleTools
-
-import YaoDenseRegister: datatype
-# APIs for cache block's matrix
-export update_cache
-
-# module APIs
-export usedbits, addrs, subblocks, block, chblock, chsubblocks, @const_gate, tokenof
-export nqubits, nactive, mat, datatype
-export iparameters, niparameters, setiparameters!, nparameters, parameters, parameter_type, iparameter_type
-export apply!, dispatch!, dispatch!!, applymatrix
-export parent, adjoint
-export blockfilter, blockfilter!, expect
-
-include("general_matrix.jl")
+include("utils.jl")
 
 include("abstract_block.jl")
-include("Sequential.jl")
-include("MatrixBlock.jl")
+include("blocktree.jl")
 
-include("Measure.jl")
-include("Function.jl")
-include("IOSyntax.jl")
+# concrete blocks
+include("matrix/matrix.jl")
 
-include("pauli_group.jl")
-include("block_operations.jl")
-include("linalg.jl")
+include("parse_block.jl")
+include("deprecations.jl")
 
-include("interfaces.jl")
-include("primitive_interface.jl")
-include("cache_interface.jl")
-include("sequence_interface.jl")
-
-using LinearAlgebra: Adjoint
-Base.:(|>)(reg::AbstractRegister, circuit::Union{AbstractBlock, Adjoint{<:Any, <:AbstractBlock}}) = apply!(reg, circuit)
-
-
-end # module
+end # YaoBlockTree
