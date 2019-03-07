9b11111fd7725ae2af25876b5b73162ea2cd4faf
Roger-luo <hiroger@qq.com>
2019-03-08 03:21:48 +0800

    update compat

--- src/compat.jl
+++ src/compat.jl
@@ -1,11 +1,4 @@
 using TupleTools
 
 diff(v::AbstractVector) = Base.diff(v)
-
-@static if isdefined(TupleTools, :diff)
-    diff(v::Tuple) = TupleTools.diff(v)
-else
-    diff(v::Tuple{}) = () # similar to diff([])
-    diff(v::Tuple{Any}) = ()
-    diff(v::Tuple) = (v[2]-v[1], diff(Base.tail(v))...)
-end
+diff(v::Tuple) = TupleTools.diff(v)
