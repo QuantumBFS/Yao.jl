d26c2ae8a6cb543c7dae7282c2c39396e94e5e89
Roger-luo <hiroger@qq.com>
2019-03-17 17:58:52 +0800

    delete matrix block

--- test/matrix/matrix.jl
+++
@@ -1,9 +0,0 @@
-using Test, YaoBlockTree
-
-@testset "test primitive block" begin
-    include("primitive/primitive.jl")
-end
-
-@testset "test composite block" begin
-    include("composite/composite.jl")
-end
