c8a952c29a6a73571bb32a219310b748e4822a18
Roger-luo <hiroger@qq.com>
2019-03-22 03:38:08 +0800

    add measure block

---
+++ src/primitive/measure.jl
@@ -0,0 +1,42 @@
+export measure, Measure
+
+mutable struct Measure{N, T, K} <: AbstractBlock{N, T}
+    locations::NTuple{K, Int}
+    collapseto::Union{Int, Nothing}
+    remove::Bool
+    results::Vector{Int}
+    Measure{N, T}(locations::NTuple{K, Int}, collapseto, remove) where {N, K, T} =
+        new{N, T, K}(locations, collapseto, remove)
+end
+
+function YaoBase.measure(::Type{T}, n::Int, locs::NTuple{K, Int}; collapseto=nothing, remove=false) where {K, T}
+    Measure{n, T}(locs, collapseto, remove)
+end
+
+# NOTE: make sure this won't overwrite YaoBase.measure
+YaoBase.measure(n::Int, locs::NTuple{K, Int}; collapseto=nothing, remove=false) =
+    measure(ComplexF64, n, locs; collapseto=collapseto, remove=remove)
+YaoBase.measure(locs::Int...; collapseto=nothing, remove=false) =
+    @λ(n->measure(n, locs; collapseto=collapseto, remove=remove))
+
+function apply!(r::AbstractRegister, m::Measure{0})
+    if m.collapseto !== nothing
+        m.results = measure_setto!(r; bit_config=r.collapseto)
+    elseif m.remove
+        m.results = measure_remove!(r)
+    else
+        m.results = measure!(r)
+    end
+    return m
+end
+
+function apply!(r::AbstractRegister, m::Measure)
+    if m.collapseto !== nothing
+        m.results = measure_setto!(r, m.locations; bit_config=r.collapseto)
+    elseif m.remove
+        m.results = measure_remove!(r, m.locations)
+    else
+        m.results = measure!(r, m.locations)
+    end
+    return m
+end
