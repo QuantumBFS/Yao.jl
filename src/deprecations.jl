bba75ea1e71b27b479bda575e0f5e96021e31826
Roger-luo <hiroger@qq.com>
2019-03-27 00:04:14 +0800

    mv deprecations to Yao

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,11 +1,1 @@
-@deprecate usedbits(block::AbstractBlock{N}) where N occupied_locs(block)
-
-# Originally addrs means return the addression stored in a composite block
-# this is not necessary since not all composite block store the address, e.g
-# roller, etc.
-# @deprecate addrs(block::AbstractBlock) occupied_locs(block)
-
-# NOTE: block is frequently used as variable name, to make sure there is
-#       no conflicts, this is commented.
-# @deprecate block(x::AbstractContainer) = parent(x)
-# @deprecate chblock(x::AbstractContainer, blk::AbstractBlock) = chsubblocks(x, blk)
+# deprecations
