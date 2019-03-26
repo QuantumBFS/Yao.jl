3981e8209089e8dbcde968b5ba630a291b41a9e8
Roger-luo <hiroger@qq.com>
2019-03-26 23:07:54 +0800

    update description

--- src/Yao.jl
+++ src/Yao.jl
@@ -12,9 +12,13 @@ Extensible Framework for Quantum Algorithm Design for Humans.
 
 简单易用可扩展的量子算法设计框架。
 
-幺 means unitary in Chinese.
+幺 means normalized but not orthogonal in Chinese.
 """
 const 幺 = Yao
 
 using Reexport
+
+
+using YaoBase, YaoArrayRegister, YaoBlocks
+
 end # module
