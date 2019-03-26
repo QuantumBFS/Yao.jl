7bbb1f2b7582e84cc21ac5ee794246d73464e2a3
Roger-luo <hiroger@qq.com>
2019-03-27 01:11:59 +0800

    mv deprecations to Yao

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,4 +1,1 @@
-@deprecate register(raw::AbstractMatrix; B=size(raw,2)) ArrayReg{B}(raw)
-@deprecate register(raw::AbstractVector) ArrayReg(raw)
-@deprecate register(bits::BitStr, nbatch::Int=1) ArrayReg{nbatch}(bits)
-@deprecate register(::Type{T}, bits::BitStr, nbatch::Int) where T ArrayReg{nbatch}(T, bits)
+# deprecations
