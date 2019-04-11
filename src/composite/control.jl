e7c81cc8b814f8edaa3cecf2d1f2715954c1cc4e
Roger-luo <hiroger@qq.com>
2019-04-12 06:27:03 +0800

    better error msg

--- src/composite/control.jl
+++ src/composite/control.jl
@@ -11,6 +11,7 @@ struct ControlBlock{N, BT<:AbstractBlock, C, M, T} <: AbstractContainer{N, T, BT
     function ControlBlock{N, BT, C, M, T}(ctrl_locs, ctrl_config, block, locs) where {N, C, M, T, BT<:AbstractBlock}
         @assert_locs N (ctrl_locs..., locs...)
         @assert nqubits(block) == M "number of locations doesn't match the size of block"
+        @assert block isa AbstractBlock "expect a block, got $(typeof(block))"
         new{N, BT, C, M, T}(ctrl_locs, ctrl_config, block, locs)
     end
 end
