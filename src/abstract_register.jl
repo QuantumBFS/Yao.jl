4da0b1d872d86a55ec237fe504f84405ebe4c599
GiggleLiu <cacate0129@gmail.com>
2019-04-26 02:13:57 +0800

    fix docstring

--- src/abstract_register.jl
+++ src/abstract_register.jl
@@ -74,8 +74,8 @@ If only an integer is provided, then returns a lambda function.
 addbits!(n::Int) = @λ(register -> addbits!(register, n))
 
 """
-    insert_qubits!(register, loc::Int; n::Int=1) -> register
-    insert_qubits!(loc::Int; n::Int=1) -> λ(register)
+    insert_qubits!(register, loc::Int; nqubits::Int=1) -> register
+    insert_qubits!(loc::Int; nqubits::Int=1) -> λ(register)
 
 Insert `n` qubits to given register in state |0>.
 i.e. |psi> -> |psi> ⊗ |000> ⊗ |psi>, increased bits have higher indices.
