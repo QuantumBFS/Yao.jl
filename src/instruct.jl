197778389b7f94b873e63d0a1110a954d0fd5650
Roger-luo <hiroger@qq.com>
2019-05-02 22:00:43 +0800

    fix https://github.com/QuantumBFS/Yao.jl/issues/189

--- src/instruct.jl
+++ src/instruct.jl
@@ -73,6 +73,7 @@ end
 
 YaoBase.instruct!(state::AbstractVecOrMat, U::IMatrix, locs::NTuple{N, Int}) where N = state
 YaoBase.instruct!(state::AbstractVecOrMat, U::IMatrix, locs::Int)  = state
+YaoBase.instruct!(state::AbstractVecOrMat, U::IMatrix, locs::Tuple{Int}) = state
 
 # one-qubit instruction
 YaoBase.instruct!(state::AbstractVecOrMat{T}, g::AbstractMatrix{T}, locs::Tuple{Int}) where T =
