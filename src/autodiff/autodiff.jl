59c841b5f3666354cde7c2be3bd21ace60ff371a
Rogerluo <hiroger@qq.com>
2019-09-24 01:34:51 -0400

    add more docs (#73)

--- src/autodiff/autodiff.jl
+++ src/autodiff/autodiff.jl
@@ -1,3 +1,14 @@
+export AD
+
+"""
+    AD
+
+This module provides the basic routines and syntax for automatic differentiation (AD).
+However, one need to port this to AD frameworks like [Zygote](https://github.com/FluxML/Zygote.jl),
+[Tracker](https://github.com/FluxML/Tracker.jl) or [Yota](https://github.com/dfdx/Yota.jl) to make
+it composable with a general Julia program. You can also check the work-in-progress package
+[YaoAD](https://github.com/QuantumBFS/YaoAD.jl) that provides this functionality.
+"""
 module AD
 
 using BitBasis, YaoArrayRegister, YaoBase
