38bc4803b6dce0064469325559b6c1c77c9db5a1
Roger-luo <hiroger@qq.com>
2019-10-01 14:52:29 -0400

    fix test & bugs

--- src/engine/types.jl
+++ src/engine/types.jl
@@ -65,3 +65,48 @@ function parse_sym(ex::Expr)
         throw(Meta.ParseError("invalid syntax $ex"))
     end
 end
+
+
+using .Simplify.SpecialSets
+using .Simplify: set_context!, Context, Associative, Commutative
+
+# we only consider them as scalars numbers here
+set_context!(Context([
+    Associative.([
+        +,
+        *,
+        &,
+        |,
+        ⊻,
+    ]);
+    Commutative.([
+        +,
+        *,
+        &,
+        |,
+        ⊻,
+    ]);
+    Closure(+, TypeSet(Number))
+    Closure(+, TypeSet(Int))
+    Closure(-, TypeSet(Number))
+    Closure(-, TypeSet(Int))
+    Closure(*, TypeSet(Number))
+    Closure(*, TypeSet(Int))
+    Signature(inv, [TypeSet(Number)], TypeSet(Number))
+    Signature(/, [TypeSet(Number), TypeSet(Number)], TypeSet(Float64))
+    Signature(^, [Positive, TypeSet(Number)], Positive)
+    Signature(^, [Zero, TypeSet(Number)], Set([0, 1]))
+    Signature(^, [TypeSet(Number), Even], Nonnegative)
+    Signature(^, [TypeSet(Real), TypeSet(Real)], TypeSet(Real))
+    Signature(^, [TypeSet(Number), TypeSet(Number)], TypeSet(Number))
+    Signature(abs, [TypeSet(Number)], Nonnegative)
+    Signature(sqrt, [TypeSet(Real)], Nonnegative)
+    Signature(sin, [TypeSet(Real)], GreaterThan{Number}(-1, true) ∩ LessThan{Number}(1, true))
+    Signature(cos, [TypeSet(Real)], GreaterThan{Number}(-1, true) ∩ LessThan{Number}(1, true))
+    Signature(tan, [TypeSet(Number)], TypeSet(Number))
+    Signature(log, [TypeSet(Number)], TypeSet(Float64))
+    Signature(diff, [TypeSet(Number), TypeSet(Number)], TypeSet(Number))  # FIXME
+    Closure(&, TypeSet(Bool))
+    Closure(|, TypeSet(Bool))
+    Signature(!, [TypeSet(Bool)], TypeSet(Bool))
+]))
