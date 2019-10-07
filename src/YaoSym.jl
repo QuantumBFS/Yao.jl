f92d6ed6e9a556f4b32ef3d6982fe0845a0005de
Roger-luo <hiroger@qq.com>
2019-10-07 01:47:38 -0400

    rm conditional

--- src/YaoSym.jl
+++ src/YaoSym.jl
@@ -1,11 +1,6 @@
 module YaoSym
 
 include("register.jl")
-
-using Requires
-
-@init @require SymEngine="123dc426-2d89-5057-bbad-38513e3affd8" begin
-    include("symengine/backend.jl")
-end
+include("symengine/backend.jl")
 
 end # module
