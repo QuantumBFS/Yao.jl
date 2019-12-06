22f1673e5e0aa3ad5b61149ad1fc8ec6cab8a4db
Roger-luo <hiroger@qq.com>
2019-12-06 05:28:56 -0500

    polish link

--- make.jl
+++ make.jl
@@ -20,11 +20,11 @@ const top_nav = """
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="$base_url/tutorials">Tutorial<span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="#">Tutorial<span class="sr-only">(current)</span></a>
         </li>
 
         <li class="nav-item">
-            <a class="nav-link" href="https://docs.yaoquantum.org/">Documentation</a>
+            <a class="nav-link" href="https://docs.yaoquantum.org/dev">Documentation</a>
         </li>
 
         <li class="nav-item">
