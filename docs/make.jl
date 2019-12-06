eface7e647e9053af740ebb1d8f40243dc21a9bc
Roger-luo <hiroger@qq.com>
2019-12-06 05:29:35 -0500

    polish link

--- docs/make.jl
+++ docs/make.jl
@@ -21,7 +21,7 @@ const top_nav = """
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <a class="nav-link" href="$base_url/tutorials">Tutorial</a>
+          <a class="nav-link" href="$base_url/tutorials/dev">Tutorial</a>
         </li>
 
         <li class="nav-item active">
