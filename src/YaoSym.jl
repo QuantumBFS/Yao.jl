56a7f8042f31691a50f140a2de4bfa8c02cf9f81
Rogerluo <rogerluo.rl18@gmail.com>
2020-03-04 19:48:29 -0500

    Format .jl files (#25)

--- src/YaoSym.jl
+++ src/YaoSym.jl
@@ -5,7 +5,7 @@ using Requires
 include("register.jl")
 
 function __init__()
-    @require SymEngine="123dc426-2d89-5057-bbad-38513e3affd8" include("symengine/backend.jl")
+    @require SymEngine = "123dc426-2d89-5057-bbad-38513e3affd8" include("symengine/backend.jl")
 end
 
 end # module
