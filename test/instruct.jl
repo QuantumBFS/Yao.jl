bbf13172e4d40a3cfe688ac8a01a7d5d4464e947
Roger-luo <rogerluo.rl18@gmail.com>
2021-02-20 15:44:49 -0500

    fix test on 1.6

--- test/instruct.jl
+++ test/instruct.jl
@@ -39,10 +39,17 @@ end
 
 @testset "test auto conversion" begin
     v = rand(ComplexF32, 1 << 8)
-    @test_logs (
-        :warn,
-        "Element Type Mismatch: register Complex{Float32}, operator Complex{Float64}. Converting operator to match, this may cause performance issue",
-    ) instruct!(v, Const.CNOT, (1, 2))
+    if VERSION < v"1.6"
+        @test_logs (
+                :warn,
+                "Element Type Mismatch: register Complex{Float32}, operator Complex{Float64}. Converting operator to match, this may cause performance issue",
+        ) instruct!(v, Const.CNOT, (1, 2))
+    else
+        @test_logs (
+                :warn,
+                "Element Type Mismatch: register ComplexF32, operator ComplexF64. Converting operator to match, this may cause performance issue",
+        ) instruct!(v, Const.CNOT, (1, 2))
+    end
 end
 
 
