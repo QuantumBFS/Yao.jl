3e3a35667ba158687f95e235cf1f01e2e9bda353
Roger-Luo <rogerluo.rl18@gmail.com>
2021-06-01 22:01:43 -0400

    fix legible lambda

--- src/abstract_register.jl
+++ src/abstract_register.jl
@@ -70,9 +70,8 @@ function relax!(locs::NTuple{N,Int}; to_nactive::Union{Nothing,Int} = nothing) w
         return LegibleLambda("(register->relax!(register, locs...; to_nactive))", lambda)
     else
         return LegibleLambda(
-            lambda,
             :(register -> relax!(register, locs...; to_nactive)),
-            Dict(:locs => locs, :to_nactive => to_nactive),
+            lambda,
         )
     end
 end
