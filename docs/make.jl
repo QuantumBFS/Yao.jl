98f911043f7aad99b1348cc69f54a7efaea6c7d3
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-06-08 19:19:39 +0530

    Better display of docs (#283)
    * Create send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Delete send_docs_to_site.yml
    
    * Add files via upload
    
    * Update make.jl
    
    * Update Project.toml
    
    * Add files via upload
    
    * Update make.jl
    
    * Update make.jl
    
    * Delete quantumbfs-darkdefs.scss
    
    * Delete quantumbfs-lightdefs.scss
    
    * Delete quantumbfs-style.scss
    
    * Create indigo.css
    
    * Update make.jl
    
    * Update .gitignore
    
    * Update make.jl
    
    * Update make.jl
    
    * Update Project.toml
    
    * Update make.jl
    
    * Delete logo.png
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,82 +7,9 @@ using YaoBlocks: Optimise
 using Documenter.Writers.HTMLWriter
 using Documenter.Utilities.DOM
 using Documenter.Utilities.DOM: Tag, @tags
-# Evil Prirate
+#Venerable Inventor :)
 
-const base_url = raw"https://yaoquantum.org"
-
-const top_nav = """
-<div id="top" class="navbar-wrapper">
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="$base_url">
-        <img src="$base_url/assets/images/logo-light.png">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="$base_url/tutorials/dev">Tutorial</a>
-        </li>
-
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Documentation<span class="sr-only">(current)</span></a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="$base_url/benchmark">Benchmark</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="http://yaoquantum.org/soc">SoC</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="http://yaoquantum.org/research">Research</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="https://github.com/QuantumBFS/Yao.jl">GitHub</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link" href="https://github.com/QuantumBFS/Yao.jl/blob/master/CONTRIBUTING.md">Contribute</a>
-        </li>
-      </ul>
-    </div>
-</nav>
-</div>
-"""
-
-function HTMLWriter.render_html(
-    ctx,
-    navnode,
-    head,
-    sidebar,
-    navbar,
-    article,
-    footer,
-    scripts::Vector{DOM.Node} = DOM.Node[],
-)
-    @tags html body div
-    DOM.HTMLDocument(
-        html[:lang=>"en"](
-            head,
-            body(
-                Tag(Symbol("#RAW#"))(top_nav),
-                div[".documenter-wrapper#documenter"](
-                    sidebar,
-                    div[".docs-main"](navbar, article, footer),
-                    HTMLWriter.render_settings(ctx),
-                ),
-            ),
-            scripts...,
-        ),
-    )
-end
-
+download("yaoquantum.org/assets/logo-light.png", "docs/src/assets/logo.png")
 
 const PAGES = [
     "Home" => "index.md",
@@ -107,12 +34,8 @@ makedocs(
         prettyurls = ("deploy" in ARGS),
         canonical = ("deploy" in ARGS) ? "https://docs.yaoquantum.org/" : nothing,
         assets = [
-            "assets/main.css",
-            asset("https://yaoquantum.org/assets/main.css"),
-            asset("http://yaoquantum.org/favicon.ico"),
-            asset(
-                "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
-            ),
+            "assets/themes/indigo.css",
+            asset("https://yaoquantum.org/assets/favicon-light.ico", class=:ico),
         ],
     ),
     doctest = ("doctest=true" in ARGS),
