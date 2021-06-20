ebf61cfd678b6893570d33f86de897e92e8eaa80
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-06-20 22:36:40 +0530

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -62,9 +62,9 @@ for i in x
          // Where to grab the headings to build the table of contents.
          contentSelector: '.js-toc-content',
          // Which headings to grab inside of the contentSelector element.
-         headingSelector: 'h1, h2, h3, h4',
+         headingSelector: 'h1, h2, h3',
          // For headings inside relative or absolute positioned containers within content.
-         hasInnerContainers: true,
+         hasInnerContainers: false,
        });
        </script></body>""")  
 		
