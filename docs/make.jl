3030fb05f60f0b1a25ff1c256ddc8a814b36a144
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-06-25 00:32:48 +0530

    Injecting the ToC into Yao docs (#284)
    * Create send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Update send_docs_to_site.yml
    
    * Delete send_docs_to_site.yml
    
    * Add files via upload
    
    * Update make.jl
    
    * Update Project.toml
    
    * Add files via upload
    
    * Update make.jl
    
    * Update make.jl
    
    * Delete quantumbfs-darkdefs.scss
    
    * Delete quantumbfs-lightdefs.scss
    
    * Delete quantumbfs-style.scss
    
    * Create indigo.css
    
    * Update make.jl
    
    * Update .gitignore
    
    * Update make.jl
    
    * Update make.jl
    
    * Update Project.toml
    
    * Update make.jl
    
    * Delete logo.png
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl
    
    * Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -45,4 +45,35 @@ makedocs(
     pages = PAGES,
 )
 
-deploydocs(repo = "github.com/QuantumBFS/Yao.jl.git", target = "build")
+x = []
+for (root, dirs, files) in walkdir("docs/build")
+           global x = [x; joinpath.(root, files)] # files is a Vector{String}, can be empty
+end
+
+for i in x
+	if(endswith(i, ".html"))
+		y = read(i, String)
+		y = replace(y, """<body><div id="documenter">""" => """<body><div id="documenter"><div class="js-toc" style="right: 0;height: 0px;min-width: 25rem;z-index: 10;display: block;position: fixed; top: 0"></div>""")
+		y = replace(y, """</head>""" => """<link href="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.11.1/tocbot.css" rel="stylesheet" type="text/css"/><style> .toc-list { padding-left: 20px; } @media only screen and (min-width: 1841px) { .docs-main { margin-left: 40rem !important } } @media only screen and (max-width: 1589px) { .js-toc { display: none !important; } } </style></head>""")
+		y = replace(y, """</body>""" => """<script src="https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.11.1/tocbot.min.js"></script><script>
+       tocbot.init({
+         // Where to render the table of contents.
+         tocSelector: '.js-toc',
+         // Where to grab the headings to build the table of contents.
+         contentSelector: '.js-toc-content',
+         // Which headings to grab inside of the contentSelector element.
+         headingSelector: 'h1, h2, h3, h4',
+         // For headings inside relative or absolute positioned containers within content.
+         hasInnerContainers: false,
+	 orderedList: false,
+       });
+       </script></body>""")  
+		
+		y = replace(y, """<div class="docs-main">""" => """<div class="js-toc-content docs-main">""")
+		f = open(i, "w")
+		write(f, y)
+		close(f)
+	end
+end
+
+deploydocs(repo = "github.com/VarLad/Yao.jl.git", target = "build")
