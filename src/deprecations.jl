84fee40fa2e838a65c267e3debd0ef7262052562
Rogerluo <rogerluo.rl18@gmail.com>
2022-02-25 09:34:14 -0500

    fix warning & rm wrong depwarn (#339)

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,3 +1,2 @@
-import YaoArrayRegister: insert_qudits!, ArrayReg
-@deprecate insert_qubits!(args...; kwargs...) insert_qudits!(args...; kwargs...)
+import YaoArrayRegister: ArrayReg
 @deprecate ArrayReg(bitstr::BitStr; nbatch::Union{Int,NoBatch}=NoBatch())  arrayreg(ComplexF64, bitstr; nbatch=nbatch)
