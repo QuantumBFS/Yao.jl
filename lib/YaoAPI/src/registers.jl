cd20c0ff971b186f78fce8fc49c49b3de4b1e657
Roger-luo <rogerluo.rl18@gmail.com>
2022-04-27 23:52:13 -0400

    fix doctest

--- lib/YaoAPI/src/registers.jl
+++ lib/YaoAPI/src/registers.jl
@@ -696,13 +696,13 @@ ArrayReg{2, ComplexF64, Array...}
     active qubits: 1/2
     nlevel: 2
 
-julia> isapprox(expect(Z + Y, preg), 0.0; rtol=1e-10)
+julia> isapprox(expect(Z + Y, preg), 0.0; atol=1e-10)
 true
 
-julia> isapprox(expect(Z + Y, r), 0.0; rtol=1e-10)
+julia> isapprox(expect(Z + Y, r), 0.0; atol=1e-10)
 true
 
-julia> isapprox(expect(put(3, 2=>(Z + Y)), reg); rtol=1e-10)
+julia> isapprox(expect(put(3, 2=>(Z + Y)), reg); atol=1e-10)
 true
 ```
 """
