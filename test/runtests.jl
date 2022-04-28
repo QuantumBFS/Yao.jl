f1626217a939cac2cd66d1b9954da00c6b1b6875
Roger-luo <rogerluo.rl18@gmail.com>
2022-04-27 21:44:45 -0400

    add setup for doctest

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,8 @@ using Random
     include("easybuild/easybuild.jl")
 end
 
+DocMeta.setdocmeta!(Yao, :DocTestSetup, :(using Yao, YaoAPI, YaoArrayRegister, YaoBlocks, YaoSym); recursive=true)
+
 Documenter.doctest(YaoAPI; manual=false)
 Documenter.doctest(YaoArrayRegister; manual=false)
 Documenter.doctest(YaoBlocks; manual=false)
