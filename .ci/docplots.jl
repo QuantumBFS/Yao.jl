324ca28934df4b295cc0c39eadb1a628f0d0a159
GiggleLiu <cacate0129@gmail.com>
2022-04-29 22:44:28 -0400

    save

--- .ci/docplots.jl
+++ .ci/docplots.jl
@@ -3,6 +3,30 @@ using Comonicon, TikzGenerator
 using Yao
 
 imgpath(filename) = pkgdir(Yao, "docs", "src", "assets", filename)
+function brace!(c::Canvas, start, stop, str::String; mirror::Bool, draw="black", thick=true)
+    edge!(c, start, stop; decoration="{brace,$(mirror ? "mirror" : "")raise=0.5cm}",decorate=true, thick, draw)
+end
+
+function matrix!(c::Canvas, offset::Tuple, size::Tuple; text_row="", text_col="")
+    dx, dy = offset
+    X, Y = size
+    rectangle!(c, dx, dy, dx+X, dy+Y; draw="", line_width=0.05, pattern="grid", fill=false)
+    text!(c, dx-1, dy+Y/2, text_row)
+    brace!(c, (dx, dy), (dx, dy+Y); mirror=false)
+    text!(c, dx+X/2, dy-1, text_col)
+    brace!(c, (dx, dy), (dx+X, dy); mirror=true)
+end
+
+@cast function regstorage()
+    canvas(imgpath("regstorage.svg"); libs=["patterns","decorations.pathreplacing"]) do c
+        # output register
+        matrix!(c, (0, 0), (5, 3); text_row=raw"\large $2^a$", text_col=raw"\large $2^r \times b$")
+        # input register
+        matrix!(c, (0, 0), (5, 3); text_row=raw"\large $2^a$", text_col=raw"\large $2^r \times b$")
+        # input operator
+        matrix!(c, (0, 0); text_row=raw"\large $2^a$", text_col=raw"\large $2^r \times b$")
+    end
+end
 
 @cast function regstorage()
     Y = 3.0
@@ -10,7 +34,5 @@ imgpath(filename) = pkgdir(Yao, "docs", "src", "assets", filename)
         rectangle!(c, 0, 0, 5, Y; draw="", line_width=0.05, pattern="grid", fill=false)
         text!(c, -1, Y/2, raw"\large $2^a$")
         text!(c, 2.5, -1, raw"\large $2^r \times b$")
-        edge!(c, (0, 0.0), (0, Y); decoration="{brace,raise=0.5cm}",decorate=true, thick=true, draw="black")
-        edge!(c, (0.0, 0), (5.0, 0); decoration="{brace,mirror,raise=0.5cm}",decorate=true,thick=true, draw="black")
     end
 end
