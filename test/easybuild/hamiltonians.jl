890f6bf288e69d39405bbd814f7f0ba9a0075746
GiggleLiu <cacate0129@gmail.com>
2022-05-03 03:49:38 -0400

    update

--- test/easybuild/hamiltonians.jl
+++ test/easybuild/hamiltonians.jl
@@ -38,7 +38,9 @@ end
     h2 = rydberg_chain(nbits; Ω=Ω*exp(im*ξ), Δ=Δf*Ω, V)
     levine_pichler_pulse = chain(time_evolve(h1, 2τ), time_evolve(h2, 2τ))
     # half pulse drives |11> to |11>
-    @test isapprox(mat(levine_pichler_pulse[1])[Int(dit"11;3")+1, Int(dit"11;3")+1] |> abs, 1; atol=1e-3)
+    m11 = mat(levine_pichler_pulse[1])[Int(dit"11;3")+1, Int(dit"11;3")+1]
+    @test isapprox(m11 |> abs, 1; atol=1e-3)
+    @show angle(m11) / π
     @test mat(levine_pichler_pulse) * reg.state ≈ apply(reg, levine_pichler_pulse).state
     reg = apply(reg, levine_pichler_pulse)
     println()
@@ -52,8 +54,43 @@ end
     j = Int(dit"11;3")+1
     ang1 = angle(m[i, i]) / π
     ang2 = angle(m[j, j]) / π
+    @show abs(m[i,i])
+    @show 2*ang1, ang2
+    @test isapprox(ang2+2, 4*Δf/sqrt(Δf^2 + 2*Ω^2); rtol=1e-2)
+    @show ang1
     @show 2*ang1 - ang2-1
     # for (i,j,v) in zip(findnz(SparseMatrixCSC(round.(mat(h), digits=5)))...)
     #     abs(v) > 1e-1 && println(bases[i], "→", bases[j], "   ", v)
     # end
 end
+
+#Δ = 0.377371
+function lp(Δ::T, ξ::T) where T
+    Ω = one(T)
+    τ = T(4.29268/Ω)
+    V = T(1e3)
+    nbits = 2
+    h1 = rydberg_chain(nbits; Ω=Ω, Δ=Δ, V)
+    h2 = rydberg_chain(nbits; Ω=Ω*exp(im*ξ), Δ=Δ, V)
+    levine_pichler_pulse = chain(time_evolve(h1, 2τ), time_evolve(h2, 2τ))
+ 
+    m = mat(Complex{T}, levine_pichler_pulse)
+    i = Int(dit"01;3")+1
+    j = Int(dit"11;3")+1
+    ang1 = angle(m[i, i]) / π
+    ang2 = angle(m[j, j]) / π
+
+    #@test isapprox(ang2+2, 4*Δ/sqrt(Δf^2 + 2*Ω^2); rtol=1e-2)
+    #@show 2*ang1 - ang2-1
+    diff = mod(2*ang1 - ang2-1, 2)
+    abs(m[i,i]), abs(m[j,j]), min(abs(diff), abs(diff-2))
+end
+
+function f(x)
+    res = lp(x...)
+    (1-res[1]) + (1-res[2]) + res[3]
+end
+
+using ForwardDiff, Optim
+Base.exp(m::AbstractMatrix{Complex{T}}) where T<:ForwardDiff.Dual = YaoBlocks.ExponentialUtilities.exp_generic(m)
+opt = Optim.optimize(f, [0.4, 1.0], LBFGS(); inplace=false)
