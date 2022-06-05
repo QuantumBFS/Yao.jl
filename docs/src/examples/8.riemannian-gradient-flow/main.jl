efff92f2127454ba008ef5a1d6112ef83b51a91d
Radoica Draškić <33292604+radras@users.noreply.github.com>
2022-06-06 00:35:21 +0200

    Update main.jl

--- docs/src/examples/8.riemannian-gradient-flow/main.jl
+++ docs/src/examples/8.riemannian-gradient-flow/main.jl
@@ -1,93 +1,147 @@
-# # Riemannian gradient-flow optimizer
+# # Riemannian gradient flow optimizer
 
 # In this tutorial we will present the Riemannian gradient descent algorithm described in [Wiersema and Killoran (2022)](https://arxiv.org/pdf/2202.06976.pdf).
 # As opposed to most standard optimization algorithms that optimize parameters of variational quantum circuits,
-# this algorithm optimizes a function directly over the special unitary group by following the gradient-flow over the manifold.
+# this algorithm optimizes a function directly over the special unitary group by following the gradient flow over the manifold.
+# Let's start by importing the necessary packages.
 
-using Yao, Yao.EasyBuild
+using Yao, Yao.EasyBuild, Plots, Random
 using KrylovKit: eigsolve
 
-# Utility function for generation of Pauli operators
+# Variational quantum eigensolver (VQE) is one of the most celebrated near-term quantum algorithms.
+# In the usual setting, VQE tries to reach the ground state by minimizing the energy cost function
+# ```math
+# \mathcal{L}(\theta) = \text{Tr}\{HU(\theta)\rho_0U^\dagger(\theta)\},
+# ```
+# with respect to parameters ``\theta`` which parameterize a quantum circuit ``U(\theta)``, 
+# where ``\rho_0 = |\psi_0\rangle\langle\psi_0|`` is some initial state and ``H`` the Hamiltonian whose ground state we want to approximate.
+# We can solve the optimization problem ``\text{min}_\theta\mathcal{L}(\theta)`` by following 
+# the direction of the steepest descent in parameter space which is given by the gradient of the cost function,
+# i.e. by considering the following gradient flow
+# ```math
+# \dot{\theta} = -\text{grad}\mathcal{L}(\theta).
+# ```
+# Discretizing the equation above, we recover the well-known gradient descent algorithm
+# ```math
+# \theta_{k + 1} = \theta_k - \alpha\text{grad}\mathcal{L}(\theta),
+# ```
+# where ``\alpha`` is the learning rate.
+# Let's demonstrate it on the example of finding the ground state of the transverse field Ising model.
 
-function generate_paulis(n)
-    ans = []
-    for i=1:4^n-1
-        tmp = i
-        pauli_string = []
-        for j=1:n
-            if tmp % 4 == 0
-                push!(pauli_string, j => I2)
-            elseif tmp % 4 == 1
-                push!(pauli_string, j => X)
-            elseif tmp % 4 == 2
-                push!(pauli_string, j => Y)
-            else
-                push!(pauli_string, j => Z)
-            end
-            tmp ÷= 4
-        end
-        push!(ans, kron(n, pauli_string...))
-    end
-    ans
-end;
-  
-# Calculating expansion coefficients
-  
-function calculate_omegas(n, reg, H_prob)
-    paulis = generate_paulis(n)
-    omegas = []
-    for P in paulis
-        push!(omegas, 0.5 * real(expect(H_prob, reg => time_evolve(P, π/4)) - expect(H_prob, reg => time_evolve(P, -π/4))))
-    end
-    omegas, paulis
-end;
-    
-# ## Algorithm description
+n = 8
+h = transverse_ising(n, 1.0)
+w, v = eigsolve(mat(h), 1, :SR, ishermitian=true)
+w[1]
 
-# Let us consider the VQE cost function directly over the special unitary group ``\mathcal{L}: \text{SU}(N) \rightarrow \mathbb{R}``
+Random.seed!(0)
+circuit = dispatch!(variational_circuit(n, 100), :random);
+history = Float64[]
+for i in 1:100
+    _, grad = expect'(h, zero_state(n) => circuit)
+    dispatch!(-, circuit, 0.01 * grad)
+    push!(history, real.(expect(h, zero_state(n)=>circuit)))
+end
+
+plot(history, legend=false)
+plot!(1:100, [w[1] for i=1:100])
+xlabel!("steps")
+ylabel!("energy")
+
+# Let's now consider the energy cost function directly over the special unitary group ``\mathcal{L}: \text{SU}(2^n) \rightarrow \mathbb{R}``
 # ```math
 # \mathcal{L}(U) = \text{Tr}\{HU\rho_0U^\dagger\}.
 # ```
-# To optimize the cost function we can follow the Riemannian gradient-flow defined through the following differential equation
+# To minimize the cost function we can follow the Riemannian gradient flow defined through the following differential equation
 # ```math
-# \dot{U} = \text{grad}\mathcal{L}(U) = [U\rho_0U^\dagger, H]U.
+# \dot{U} = -\text{grad}\mathcal{L}(U) = [U\rho_0U^\dagger, H]U.
 # ```
-
-# Discretizing the flow we get the following
+# Discretizing the flow we get the following recursive update rule
 # ```math
-# U_{k + 1} = \exp\{\alpha[U_k\rho_0U^\dagger_k, H]\}U_k,
+# U_{k + 1} = \exp\{\alpha[U_k\rho_0U^\dagger_k, H]\}U_k = \exp\{\alpha[\rho_k, H]\}U_k,
 # ```
-# where ``\alpha`` is appropriate learning rate.
-        
-# Now let us implement the function for a single optimization step.
+# where ``\alpha`` is the appropriate learning rate and ``U_0 = I``.
 
-function step_and_cost(n, circuit, H_prob, α)
-    omegas, paulis = calculate_omegas(n, zero_state(n) |> circuit, H_prob)
 
-    for (ω, P) in zip(omegas, paulis)
-        if abs(ω) > 1e-6
-            append!(circuit, chain(n, time_evolve(P, -ω * α)))
+# We can expand the commutator in the exponent in the basis of Pauli strings ``P^j``
+# ```math
+# [\rho_k, H] = \frac{1}{2^n}\sum_{j = 1}^{4^n - 1}\omega^j_kP^j,
+# ```
+# where
+# ```math
+# \omega^j_k = \text{Tr}\{[\rho_k, H]P^j\} = \text{Tr}\{[H, P^j]\rho_k\} = \langle[H, P^j]\rangle_{\rho_k}.
+# ```
+# It turns out that ``\omega^j_k`` can easily be evaluated with the help of a parameter shift rule
+# ```math
+# \omega^j_k = \langle[H, P^j]\rangle_{\rho_k} = -i\langle V^\dagger_j(\pi/4)HV_j(\pi/4) - V^\dagger_j(-\pi/4)HV_j(-\pi/4)\rangle_{\rho_k},
+# ```
+# where ``V_j(t) = \exp\{-itP^j\}``.
+
+# Next, we write a function for generation of 2-local Pauli operators.
+# We will restrict the Riemannian gradient to this subspace of the Lie algebra since otherwise the number 
+# of parameters to calculate would be ``4^8 - 1 = 65535`` which is too much for a reasonable runtime of the algorithm. 
+
+function generate_2local_pauli_strings(n)
+    pauli_strings = []
+    for i = 1:n
+        push!(pauli_strings, kron(n, i => X))
+        push!(pauli_strings, kron(n, i => Y))
+        push!(pauli_strings, kron(n, i => Z))
+    end
+    for i = 1:n-1
+        for j = i+1:n
+            for P1 in [X, Y, Z]
+                for P2 in [X, Y, Z]
+                    push!(pauli_strings, kron(n, i => P1, j => P2))
+                end
+            end
+        end
+    end
+    pauli_strings
+end;
+
+# Next we write functions for calculating the expansion coefficients and a single optimization step.
+# We will absorb the factor of ``1/2^n`` into the learning rate.
+  
+function calculate_omegas(n, reg, h, pauli_strings)
+    iω = []
+    for P in pauli_strings
+        push!(iω, real(expect(h, reg => time_evolve(P, π/4)) - expect(h, reg => time_evolve(P, -π/4))))
+    end
+    iω
+end;
+
+function step_and_cost!(n, circuit, h, α, pauli_strings)
+    iω = calculate_omegas(n, zero_state(n) |> circuit, h, pauli_strings)
+
+    for (iωʲ, P) in zip(iω, pauli_strings)
+        if abs(iωʲ) > 1e-6 # we will only keep the ones that actually contribute
+            append!(circuit, chain(n, time_evolve(P, -α * iωʲ)))
         end
     end
 
-    real(expect(H_prob, zero_state(n) => circuit))
+    real(expect(h, zero_state(n) => circuit))
 end;
       
-# Finally, let us try it out.
+# Finally, let's try it out.
+# We initialize the state ``|0\rangle`` and apply several optimization steps.
 
-n = 2
-H_prob = -kron(n, 1 => X) - kron(n, 2=>Z) - kron(n, 1=>Y, 2=>X)
-w, v = eigsolve(mat(H_prob), 1, :SR, ishermitian=true)
-w[1]
- 
-# We initialize the state and apply several optimization steps
-            
-circuit = chain(n, [put(n, 1 => Rx(0.1)), 
-                    put(n, 2 => Ry(0.5)),
-                    cnot(n, 1, 2),
-                    put(n, 1 => Ry(0.6))]);
+circuit = chain(n)
+pauli_strings = generate_2local_pauli_strings(n)
+history = Float64[]
 
-for i=1:10
-    cost = step_and_cost(n, circuit, H_prob, 0.1)
-    println(cost)
+for i=1:100
+    cost = step_and_cost!(n, circuit, h, 0.01, pauli_strings)
+    push!(history, cost)
 end
+
+plot(history, legend=false)
+plot!(1:100, [w[1] for i=1:100])
+xlabel!("steps")
+ylabel!("energy")
+
+# When we compare the final states achieved with the Riemannian gradient flow 
+# optimizer and with the standard VQE we can notice that the former has lower quality.
+# This is because the Riemannian gradient flow optimizer has only a local view of the cost landscape
+# while VQE can access these directions since the ansatz we used is universal.
+# However, if we were able to calculate all of the ``4^n - 1`` projections, 
+# Riemannian gradient flow optimizer would be guaranteed to converge given the appropriate learning rate!
