44e81c1aa80085335ff29b1c87d13d9cc0eb3352
Jan Lukas Bosse <janlukas.bosse@stud.uni-goettingen.de>
2023-03-08 14:46:14 +0000

    fixed docstring to make test pass

--- lib/YaoArrayRegister/src/register.jl
+++ lib/YaoArrayRegister/src/register.jl
@@ -265,9 +265,10 @@ Create a register initialized to zero from an existing one.
 ### Examples
 
 ```jldoctest; setup=:(using Yao)
-julia> reg = rand_state(3; nbatch=2)
+julia> reg = rand_state(3; nbatch=2);
+
 julia> zero_state_like(reg, 2)
-BatchedArrayReg{2, ComplexF64, Transpose...}
+BatchedArrayReg{2, ComplexF64, Array...}
     active qubits: 2/2
     nlevel: 2
     nbatch: 2
