0493ca6b1d943ec7b898d29d14f8cb5743faac02
GiggleLiu <cacate0129@gmail.com>
2024-04-17 15:00:52 +0800

    fix doctests

--- lib/YaoAPI/src/blocks.jl
+++ lib/YaoAPI/src/blocks.jl
@@ -305,21 +305,21 @@ Returns the most compact matrix form of given block, e.g
 
 ```jldoctest; setup=:(using Yao)
 julia> mat(X)
-LuxurySparse.SDPermMatrix{ComplexF64, Int64, Vector{ComplexF64}, Vector{Int64}}
-(1, 2) = 1.0 + 0.0im
-(2, 1) = 1.0 + 0.0im
+2×2 LuxurySparse.SDPermMatrix{ComplexF64, Int64, Vector{ComplexF64}, Vector{Int64}}:
+ 0.0+0.0im  1.0+0.0im
+ 1.0+0.0im  0.0+0.0im
 
 julia> mat(Float64, X)
-LuxurySparse.SDPermMatrix{Float64, Int64, Vector{Float64}, Vector{Int64}}
-(1, 2) = 1.0
-(2, 1) = 1.0
+2×2 LuxurySparse.SDPermMatrix{Float64, Int64, Vector{Float64}, Vector{Int64}}:
+ 0.0  1.0
+ 1.0  0.0
 
 julia> mat(kron(X, X))
-LuxurySparse.SDPermMatrix{ComplexF64, Int64, Vector{ComplexF64}, Vector{Int64}}
-(1, 4) = 1.0 + 0.0im
-(2, 3) = 1.0 + 0.0im
-(3, 2) = 1.0 + 0.0im
-(4, 1) = 1.0 + 0.0im
+4×4 LuxurySparse.SDPermMatrix{ComplexF64, Int64, Vector{ComplexF64}, Vector{Int64}}:
+ 0.0+0.0im  0.0+0.0im  0.0+0.0im  1.0+0.0im
+ 0.0+0.0im  0.0+0.0im  1.0+0.0im  0.0+0.0im
+ 0.0+0.0im  1.0+0.0im  0.0+0.0im  0.0+0.0im
+ 1.0+0.0im  0.0+0.0im  0.0+0.0im  0.0+0.0im
 
 julia> mat(kron(X, X) + put(2, 1=>X))
 4×4 SparseMatrixCSC{ComplexF64, Int64} with 8 stored entries:
